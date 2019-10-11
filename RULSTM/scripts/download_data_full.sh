214b14b762ba14741e7bacbfbca4b93886465d5f
Antonino Furnari <antonino.furnari@gmail.com>
2019-10-11 10:54:19 +0200

    Create download_data_full.sh

---
+++ RULSTM/scripts/download_data_full.sh
@@ -0,0 +1,6 @@
+mkdir data/rgb
+mkdir data/flow
+mkdir data/obj
+curl https://iplab.dmi.unict.it/rulstm/downloads/features/rgb_full/data.mdb -o data/rgb/data.mdb
+curl https://iplab.dmi.unict.it/rulstm/downloads/features/flow_full/data.mdb -o data/flow/data.mdb
+curl https://iplab.dmi.unict.it/rulstm/downloads/features/obj_full/data.mdb -o data/obj/data.mdb
