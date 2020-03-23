3406e34ad884f0a26bb8bfe86acc13d99924692e
Antonino Furnari <antonino.furnari@gmail.com>
2020-03-23 11:09:27 +0100

    Update download_models.sh
    moved files to other location

--- FEATEXT/scripts/download_models.sh
+++ FEATEXT/scripts/download_models.sh
@@ -1,3 +1,3 @@
 mkdir models
-wget -P models/ http://iplab.dmi.unict.it/rulstm/downloads/TSN-rgb.pth.tar
-wget -P models/ http://iplab.dmi.unict.it/rulstm/downloads/TSN-flow.pth.tar
+wget -P models/ http://iplab.dmi.unict.it/sharing/rulstm/TSN-rgb.pth.tar
+wget -P models/ http://iplab.dmi.unict.it/sharing/rulstm/TSN-flow.pth.tar
